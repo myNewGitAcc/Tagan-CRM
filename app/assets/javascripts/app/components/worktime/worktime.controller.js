1f0ca28f688d0c9e9a3fec58cf4b90d3e43bbc06
lexa-hacker <ambassadortsure@gmail.com>
2016-09-12 23:44:11 +0300

    refactoring of controller and template in worktime

--- app/assets/javascripts/app/components/worktime/worktime.controller.js
+++ app/assets/javascripts/app/components/worktime/worktime.controller.js
@@ -6,31 +6,26 @@ class worktimeController {
     this.remainsWork = 0;
 
     users.getProfile().then( (response) => {
-      this.onlineUser = response.data.data.status;
-      this.buttonPause = this.onlineUser == 'away' ? 'Continue' : 'Pause';
+      this.userStatus = response.data.data.status;
       this.btn = true;
     });
 
-    this.pause = ()=> {
-      if (this.buttonPause == 'Pause') {
-        this.buttonPause = 'Continue';
-        users.userStatusUpdate($scope.currentUserId, 2);
-      }
-      else {
-        this.buttonPause = 'Pause';
-        users.userStatusUpdate($scope.currentUserId, 1);
-      }
+    let chngUserStatus = status => {
+      users.userStatusUpdate($scope.currentUserId, status=="offline"?0:status=="online"?1:2);
+      this.userStatus = status;
     }
 
-    this.moveAway = ()=> {
-      users.userStatusUpdate($scope.currentUserId, 0);
-      this.buttonPause = 'Pause';
-      this.onlineUser = 'offline';
+    this.pause = () => {
+      if(this.userStatus == "online")
+        chngUserStatus("away");
+      else
+        chngUserStatus("online");
     }
-
-    this.startTime = ()=> {
-      users.userStatusUpdate($scope.currentUserId, 1);
-      this.onlineUser = 'online';
+    this.moveAway = () => {
+      chngUserStatus("offline");
+    }
+    this.startTime = () => {
+      chngUserStatus("online");
     }
   }
 
