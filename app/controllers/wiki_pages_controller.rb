e565f76dce888bff163be38eb65438bc6371c52c
EvZheny <chertckov@gmail.com>
2017-09-06 11:36:00 +0300

    Fixed errors with wiki

--- app/controllers/wiki_pages_controller.rb
+++ app/controllers/wiki_pages_controller.rb
@@ -5,4 +5,37 @@ class WikiPagesController < ApplicationController
   skip_before_action :auth_user!
 
   acts_as_wiki_pages_controller
+
+  def index
+    @pages = WikiPage.all
+    render "base_wiki_pages/all"
+  end
+
+  def new
+    @page = WikiPage.new
+    render "/wiki/new"
+  end
+
+  def create
+    @page = WikiPage.create(wiki_pages_params)
+    @page.update_attribute(:path, @page.id)
+    redirect_to wiki_page_path(@page)
+  end
+
+  def show
+    @page = WikiPage.find(params[:id])
+    @page.content = @page.content.gsub('_','\_').gsub('*','\*')
+    render "base_wiki_pages/show"
+  end
+
+  def destroy
+    @page = WikiPage.find(params[:id])
+    @page.destroy
+    redirect_to action: "index"
+  end
+
+  def wiki_pages_params
+    params.require(:wiki_page).permit(:creator_id, :updator_id, :path, :title, :content, :created_at , :updated_at)
+  end
+
 end
--- app/helpers/wiki_pages_helper.rb
+++ app/helpers/wiki_pages_helper.rb
@@ -1,3 +1,8 @@
 module WikiPagesHelper
   acts_as_wiki_pages_helper
+
+  def wiki_user( user )
+    user ? user.full_name : "Guest"
+  end
+
 end
