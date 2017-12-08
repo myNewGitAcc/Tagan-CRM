9e3bf2786a9c9f451350c949b0e24ea1e2e50958
Artoym Pechoyrin <pecha7x@gmail.com>
2017-12-08 03:14:28 +0300

    Add model сandidates

---
+++ db/migrate/20171208000434_create_candidates.rb
@@ -0,0 +1,15 @@
+class CreateCandidates < ActiveRecord::Migration
+  def change
+    create_table :candidates do |t|
+      t.string  :first_name
+      t.string  :last_name
+      t.string  :middle_name
+      t.string  :email
+      t.integer :phone
+      t.text    :notes
+      t.string  :attachment
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171206141047) do
+ActiveRecord::Schema.define(version: 20171208000434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,18 @@ ActiveRecord::Schema.define(version: 20171206141047) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "candidates", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "middle_name"
+    t.string   "email"
+    t.integer  "phone"
+    t.text     "notes"
+    t.string   "attachment"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "commissions", force: :cascade do |t|
     t.integer  "company_id"
